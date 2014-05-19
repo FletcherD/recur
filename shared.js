dd24628e1b03a3ae6fdf392289b2a4a3941b85fc
Fletcher Dostie <fletcher@lantostechnologies.com>
2014-05-19 13:49:45 -0400

    JavaScript to show recently shared parameters

---
+++ shared.js
@@ -0,0 +1,27 @@
+function makePreview(data) {
+    var previewDiv = document.createElement('div'); 
+    
+    previewDiv.style.width = "50%";
+    previewDiv.style.float = "left";
+    var previewHref = document.createElement('a');
+    previewHref.href = "#";
+    previewDiv.appendChild(previewHref);
+    previewImg = new Image();
+    previewImg.src = data.fields.preview_url;
+    previewImg.addEventListener('click', function(){
+        alert(data.fields.parameter_string);
+    });
+    previewHref.appendChild(previewImg);
+    
+    previewDiv.appendChild(document.createElement('br'));
+    date = new Date(data.fields.upload_date);
+    previewDiv.appendChild(document.createTextNode(date.toLocaleString()));
+    return previewDiv;
+}
+
+function callback(data) {
+    for (var i = 0; i < data.length; i++) { 
+        previewDiv = makePreview(data[i]);
+        document.getElementById('shared').appendChild(previewDiv);
+    }
+}
